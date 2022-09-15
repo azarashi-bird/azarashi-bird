3e2dae10686144b7624f2bbe16a140124bf52448
tamaroh <tamaroh@gmail.com>
2022-09-15 17:07:20 +0900

    add typescript and other dependencies, create top page(wip)

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
-import {Provider as PaperProvider} from 'react-native-paper';
+import {
+  MD3LightTheme as DefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
 import {Button} from 'react-native-paper';
+import Top from './components/top';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  version: 3,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#f6f3cf',
+    secondary: '#76DBF7',
+    tertiary: '#a1b2c3',
+  },
+};
 
 export default function App() {
   return (
-    <PaperProvider>
-      <View style={styles.container}>
+    <PaperProvider theme="{theme}">
+      <Top></Top>
+      {/* <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Button
           icon="camera"
@@ -15,16 +32,7 @@ export default function App() {
           Press me
         </Button>
         <StatusBar style="auto" />
-      </View>
+      </View> */}
     </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
