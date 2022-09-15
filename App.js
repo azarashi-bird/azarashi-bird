e0bec02bfeabfc7f3ac685b54e14286442c6746d
kiki <k.kazaneko@gmail.com>
2022-09-15 14:10:20 +0900

    add paperProvider

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {Button} from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Button
+          icon="camera"
+          mode="contained"
+          onPress={() => console.log('Pressed')}>
+          Press me
+        </Button>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
