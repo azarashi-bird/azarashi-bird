b5c8920cfbe18fbe21497040f6fc2a0c1af5e2be
Kotaro <hideandhide35@gmail.com>
2022-09-16 09:40:23 +0900

    display bird and cage

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import {FlyingBird} from './components/FlyingBird';
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
@@ -6,16 +7,17 @@ import {Button} from 'react-native-paper';
 export default function App() {
   return (
     <PaperProvider>
-      <View style={styles.container}>
+      {/* <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Button
           icon="camera"
           mode="contained"
           onPress={() => console.log('Pressed')}>
           Press me
-        </Button>
-        <StatusBar style="auto" />
-      </View>
+        </Button> */}
+      <FlyingBird />
+      {/* <StatusBar style="auto" />
+      </View> */}
     </PaperProvider>
   );
 }
