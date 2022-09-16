9af34c3687bf3d28c056a030e8e02fa7bcb88c2b
kiki <k.kazaneko@gmail.com>
2022-09-16 09:30:45 +0900

    add log component

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {Button} from 'react-native-paper';
+import LogView from './LogView';
+// メモ：npm install @react-navigation/bottom-tabs
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
@@ -15,10 +21,28 @@ export default function App() {
           Press me
         </Button>
         <StatusBar style="auto" />
+        <LogView />
       </View>
     </PaperProvider>
   );
 }
+
+// export default function App() {
+//   return (
+//     <PaperProvider>
+//       {/* <View style={styles.container}> */}
+//       <LogView />
+//       {/* </View> */}
+//       {/* <NavigationContainer>
+//       <Tab.Navigator>
+//         <Tab.Screen name="Home" component={HomeScreen} />
+//         <Tab.Screen name="User" component={UserScreen} />
+//         <Tab.Screen name="People" component={PeopleScreen} />
+//       </Tab.Navigator>
+//     </NavigationContainer> */}
+//     </PaperProvider>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
