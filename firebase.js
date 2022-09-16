50811d9ad537ffff94bf234a79e86902b62ee4b9
yuricode1014 <yuriko19871014@gmail.com>
2022-09-16 13:58:18 +0900

    before merge

--- firebase.js
+++ firebase.js
@@ -1,21 +1,13 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from "firebase";
+import * as firebase from 'firebase';
+// require('firebase/firestore');
+import {ENV} from './ENV';
 
-// require("dotenv").config({ path: "./.env" });
-import { ENV } from "./ENV";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = ENV;
-// {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-// };
 
 // Initialize Firebase
 let app;
@@ -26,5 +18,6 @@ if (firebase.apps.length === 0) {
 }
 
 const auth = firebase.auth();
+const firestore = firebase.firestore();
 
-export { auth };
+export {auth, firestore};
