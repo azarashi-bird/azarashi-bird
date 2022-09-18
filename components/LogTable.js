821e64fe9748cd74e71b391dc3765813a1de7e4d
kiki <k.kazaneko@gmail.com>
2022-09-18 14:03:45 +0900

    styled people tab

--- components/LogTable.js
+++ components/LogTable.js
@@ -2,20 +2,33 @@ import {StyleSheet} from 'react-native';
 import {DataTable} from 'react-native-paper';
 
 export default function LogView() {
+  const sampleLog = [
+    ['公道を掃除した', '07/21'],
+    ['寄付をした', '07/10'],
+    ['歯を磨いた', '07/06'],
+  ];
+
   return (
     <DataTable style={styles.container}>
       <DataTable.Header>
         <DataTable.Title>徳</DataTable.Title>
         <DataTable.Title numeric>日付</DataTable.Title>
       </DataTable.Header>
-      <DataTable.Row>
-        <DataTable.Cell>公道を掃除した</DataTable.Cell>
-        <DataTable.Cell numeric>07/21</DataTable.Cell>
-      </DataTable.Row>
-      <DataTable.Row>
-        <DataTable.Cell>寄付をした</DataTable.Cell>
-        <DataTable.Cell numeric>07/10</DataTable.Cell>
-      </DataTable.Row>
+      {sampleLog ? (
+        sampleLog.map((doing) => {
+          return (
+            <DataTable.Row>
+              <DataTable.Cell>{doing[0]}</DataTable.Cell>
+              <DataTable.Cell numeric>{doing[1]}</DataTable.Cell>
+            </DataTable.Row>
+          );
+        })
+      ) : (
+        <DataTable.Row>
+          <DataTable.Cell>徳</DataTable.Cell>
+          <DataTable.Cell numeric>00/00</DataTable.Cell>
+        </DataTable.Row>
+      )}
     </DataTable>
   );
 }
--- components/PeopleLog.js
+++ components/PeopleLog.js
@@ -1,10 +1,38 @@
 import {StyleSheet, View, Image} from 'react-native';
 import {Text} from 'react-native-paper';
+import {DataTable} from 'react-native-paper';
 
 export default function PeopleLog() {
+  const samplePeople = [
+    ['img', 'username', '犬を助けた', '06/12'],
+    ['img2', 'Moomin', '帽子を見つけてあげた', '06/10'],
+    ['img3', 'Sloth', '苗を植えた', '06/01'],
+  ];
+
   return (
     <View style={styles.container}>
       <Text style={styles.topContent}>みんなの徳</Text>
+      <DataTable style={styles.logs}>
+        {samplePeople ? (
+          samplePeople.map((doing) => {
+            return (
+              <DataTable.Row>
+                <DataTable.Cell>{doing[0]}</DataTable.Cell>
+                <DataTable.Cell>{doing[1]}</DataTable.Cell>
+                <DataTable.Cell>{doing[2]}</DataTable.Cell>
+                <DataTable.Cell numeric>{doing[3]}</DataTable.Cell>
+              </DataTable.Row>
+            );
+          })
+        ) : (
+          <DataTable.Row>
+            <DataTable.Cell>画像</DataTable.Cell>
+            <DataTable.Cell>ユーザー名</DataTable.Cell>
+            <DataTable.Cell>徳</DataTable.Cell>
+            <DataTable.Cell numeric>00/00</DataTable.Cell>
+          </DataTable.Row>
+        )}
+      </DataTable>
     </View>
   );
 }
@@ -17,5 +45,8 @@ const styles = StyleSheet.create({
   },
   topContent: {
     marginTop: 100,
+  },
+  logs: {
+    backgroundColor: '#FFFF',
   },
 });
