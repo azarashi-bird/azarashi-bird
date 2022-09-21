e6ada3125b1c04ef07af08d27f3a029d9f86ea43
Kotaro <hideandhide35@gmail.com>
2022-09-21 15:53:27 +0900

    delete unnecessary comments

--- components/Suggest.jsx
+++ components/Suggest.jsx
@@ -1,26 +1,18 @@
-import {ScrollView, View, Button} from 'react-native';
-// import {Provider as PaperProvider} from 'react-native-paper';
-import {useNavigation} from '@react-navigation/native';
+import {View, Button} from 'react-native';
 import styles from './css';
 
 const Suggest = (props) => {
-  const navigation = useNavigation();
-
   const suggests = [
     '部屋を掃除した',
     '駅前の募金で寄付してみた',
     '歯を磨いた',
     'ゴミを分別した',
     '迷い猫を保護した',
-    // '赤ちゃんを笑顔にした',
+    '赤ちゃんを笑顔にした',
     '世界を救ってみた',
-    // '道案内をした',
-    // 'ガスの元栓を閉めた',
-    // 'お皿を洗った',
   ];
 
   return (
-    // <ScrollView>
     <View style={styles.suggestContainer}>
       {suggests.map((item, key) => (
         <Button
@@ -30,14 +22,11 @@ const Suggest = (props) => {
           onPress={(event) => {
             console.log(event);
             props.setToku(
-              // event
               event.target._internalFiberInstanceHandleDEV.child.memoizedProps
             );
-            // navigation.navigate('Top');
           }}></Button>
       ))}
     </View>
-    // </ScrollView>
   );
 };
 
