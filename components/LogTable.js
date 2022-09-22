3694327c90acebf8270ade6f8a4f803e7815cac2
kiki <k.kazaneko@gmail.com>
2022-09-22 11:19:53 +0900

    refactoring logview

--- components/LogTable.js
+++ components/LogTable.js
@@ -2,28 +2,14 @@ import {ScrollView, StyleSheet, View} from 'react-native';
 import {DataTable} from 'react-native-paper';
 import styles from './css';
 
-export default function LogView({targetTokus}) {
-  const sampleLog = [
-    ['公道を掃除した', '07/21'],
-    ['寄付をした', '07/10'],
-    ['歯を磨いた', '07/06'],
-    ['ゴミを分別した', '07/01'],
-    ['迷子を保護した', '06/25'],
-    ['赤子を拾った', '06/10'],
-    ['雛を巣に戻した', '05/25'],
-    ['道案内をした', '05/08'],
-    ['火事を防いだ', '04/01'],
-    ['みみずを土に入れた', '03/20'],
-  ];
-  // console.log(targetTokus, tokusValue);
-  const tokusArray = targetTokus.map((obj) => {
+export default function LogView({userTokus}) {
+  const tokusArray = userTokus.map((obj) => {
     const array = [];
     array.push(obj.toku);
     array.push(obj.createdAt.seconds);
     return array;
   });
 
-  // console.log(tokusArray);
   return (
     <ScrollView style={styles.tableContainer}>
       <DataTable>
--- components/LogView.js
+++ components/LogView.js
@@ -3,56 +3,36 @@ import {Text} from 'react-native-paper';
 import LogTable from './LogTable';
 import styles from './css';
 import afterViews from './afterLifes';
-import userTokus from './TargetTokus';
 import {getUserToku} from '../firebase';
 import {useEffect, useState} from 'react';
 import {useIsFocused} from '@react-navigation/native';
 
 export default function LogView() {
-  // let tokus = 7;
-  // userTokus['_3]の中身がデータ
-  const [targetTokus, updateTokus] = useState([]);
+  const [userTokus, setUserTokus] = useState([]);
   const isFocused = useIsFocused();
 
-  // const tokuDatas = userTokus['_3'];
-  // const tokusValue = tokuDatas.map((obj) => obj['toku']);
-  // const tokusNum = tokusValue.length;
   useEffect(() => {
-    const list = async () => {
+    const getUserTokus = async () => {
       const dataOfTokus = await getUserToku();
-      updateTokus(dataOfTokus);
+      setUserTokus(dataOfTokus);
     };
-    list();
+    getUserTokus();
   }, [isFocused]);
-  console.log({targetTokus});
+  console.log({userTokus});
 
   return (
     <View style={styles.logContainer}>
       <Text style={styles.topContent} variant="titleLarge">
         あなたの来世
-        <Text style={styles.strongText}>
-          {afterViews[targetTokus.length][1]}
-        </Text>
+        <Text style={styles.strongText}>{afterViews[userTokus.length][1]}</Text>
       </Text>
       <Image
-        source={afterViews[targetTokus.length][0]}
+        source={afterViews[userTokus.length][0]}
         style={styles.mainImage}></Image>
       <Text style={styles.mainText} variant="titleLarge">
-        あなたの徳　<Text style={styles.strongText}>{targetTokus.length}</Text>
-        徳
+        あなたの徳　<Text style={styles.strongText}>{userTokus.length}</Text>徳
       </Text>
-      {/* <LogTable /> */}
-      <LogTable targetTokus={targetTokus} />
+      <LogTable userTokus={userTokus} />
     </View>
   );
-  // return <Text>HHHHE</Text>;
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#F6F3CF',
-//     alignItems: 'center',
-//     justifyContent: 'flex-start',
-//   },
-// });
