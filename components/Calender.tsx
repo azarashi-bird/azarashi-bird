9d5a3d762071698e65bc7e7ab52cfb85c4f85e7f
tamaroh <tamaroh@gmail.com>
2022-09-26 21:33:39 +0900

    refactored calender.tsx a little

--- components/Calender.tsx
+++ components/Calender.tsx
@@ -5,7 +5,7 @@ import {getMonthlyToku, getUserToku} from '../firebase';
 import {useEffect, useState} from 'react';
 
 export default function Calender() {
-  const [monthlyTokus, setMonthlyTokus] = useState<any[]>([]);
+  const [monthlyTokus, setMonthlyTokus] = useState([]);
 
   let calenderData = Array(30).fill(0);
 
@@ -18,16 +18,15 @@ export default function Calender() {
   }, []);
 
   monthlyTokus.forEach((toku) => {
-    console.log(calenderData[toku.createdAt.toDate().getDate() - 1]);
     calenderData[toku.createdAt.toDate().getDate() - 1]++;
   });
-  console.log(calenderData);
+  // console.log(calenderData); <- this is logged twice
 
   return (
     <>
       <Text>今月の徳</Text>
       <View style={styles.calender}>
-        {calenderData.map((element, index) => (
+        {calenderData.map((element) => (
           <View
             style={[
               styles.calenderCell,
@@ -40,49 +39,5 @@ export default function Calender() {
         ))}
       </View>
     </>
-    // <DataTable style={styles.calender}>
-    //   <DataTable.Row style={styles.calenderRow}>
-    //     <DataTable.Cell style={styles.calenderCellEmpty} textStyle={styles.calenderText}>1</DataTable.Cell>
-    //     <DataTable.Cell style={styles.calenderCellMarked} textStyle={styles.calenderText}>2</DataTable.Cell>
-    //     <DataTable.Cell>3</DataTable.Cell>
-    //     <DataTable.Cell>4</DataTable.Cell>
-    //     <DataTable.Cell>5</DataTable.Cell>
-    //   </DataTable.Row>
-    //   <DataTable.Row>
-    //     <DataTable.Cell>1</DataTable.Cell>
-    //     <DataTable.Cell>2</DataTable.Cell>
-    //     <DataTable.Cell>3</DataTable.Cell>
-    //     <DataTable.Cell>4</DataTable.Cell>
-    //     <DataTable.Cell>5</DataTable.Cell>
-    //   </DataTable.Row>
-    //   <DataTable.Row>
-    //     <DataTable.Cell>1</DataTable.Cell>
-    //     <DataTable.Cell>2</DataTable.Cell>
-    //     <DataTable.Cell>3</DataTable.Cell>
-    //     <DataTable.Cell>4</DataTable.Cell>
-    //     <DataTable.Cell>5</DataTable.Cell>
-    //   </DataTable.Row>
-    //   <DataTable.Row>
-    //     <DataTable.Cell>1</DataTable.Cell>
-    //     <DataTable.Cell>2</DataTable.Cell>
-    //     <DataTable.Cell>3</DataTable.Cell>
-    //     <DataTable.Cell>4</DataTable.Cell>
-    //     <DataTable.Cell>5</DataTable.Cell>
-    //   </DataTable.Row>
-    //   <DataTable.Row>
-    //     <DataTable.Cell>1</DataTable.Cell>
-    //     <DataTable.Cell>2</DataTable.Cell>
-    //     <DataTable.Cell>3</DataTable.Cell>
-    //     <DataTable.Cell>4</DataTable.Cell>
-    //     <DataTable.Cell>5</DataTable.Cell>
-    //   </DataTable.Row>
-    //   <DataTable.Row>
-    //     <DataTable.Cell>1</DataTable.Cell>
-    //     <DataTable.Cell>2</DataTable.Cell>
-    //     <DataTable.Cell>3</DataTable.Cell>
-    //     <DataTable.Cell>4</DataTable.Cell>
-    //     <DataTable.Cell>5</DataTable.Cell>
-    //   </DataTable.Row>
-    // </DataTable>
   );
 }
