06da91c61f9dc29f3f28e1359dffa6fa5c3f94ac
tamaroh <tamaroh@gmail.com>
2022-09-28 15:16:52 +0900

    added unknown image

--- components/Dictionary.tsx
+++ components/Dictionary.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {ScrollView, Text, View, Image} from 'react-native';
 import styles from './css';
 import afterViews from './afterLifes';
-
+import {getUserToku} from '../firebase';
+const unknown = require('../assets/afterLifes/unknown.png');
 export default function Dictionary() {
+  const [score, setScore] = useState(0);
+  useEffect(() => {
+    async function call() {
+      const userTokus = await getUserToku();
+      setScore(Math.floor((userTokus.length % 45) / 3));
+    }
+    call();
+  });
   return (
     <View style={styles.container}>
       <Text style={styles.strongText}>図鑑</Text>
       <ScrollView contentContainerStyle={[styles.container, styles.dict]}>
         {afterViews.map((element, index) => (
           <View style={styles.dictItem}>
-            <Text style={styles.dictItemName}>{element[1]}</Text>
-            <Image source={element[0]} style={styles.dictItemImage}></Image>
+            <Text style={styles.dictItemName}>
+              {score >= index ? element[1] : '???'}
+            </Text>
+            <Image
+              source={score >= index ? element[0] : unknown}
+              style={styles.dictItemImage}></Image>
           </View>
         ))}
       </ScrollView>
