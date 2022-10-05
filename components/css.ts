30b2bef39e43b4af33863503fcf8c37859981edb
Kotaro <hideandhide35@gmail.com>
2022-10-05 11:31:44 +0900

    fix scroll bar

--- components/css.ts
+++ components/css.ts
@@ -32,12 +32,14 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#F6F3CF',
     flexGrow: 1,
+    flex: 1,
   },
 
   inner: {
     top: 30,
     bottom: 30,
     alignItems: 'center',
+    paddingBottom: 100,
     justifyContent: 'flex-start',
   },
 
@@ -188,7 +190,7 @@ const styles = StyleSheet.create({
   //// Calender
 
   calender: {
-    width: '85%',
+    width: '80%',
     borderWidth: 0,
     borderColor: 'white',
     flexDirection: 'row',
@@ -358,6 +360,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
     flexWrap: 'wrap',
+    paddingBottom: 100,
   },
   dictItem: {
     width: '30%',
@@ -447,7 +450,24 @@ export const customStyles = StyleSheet.create(
           height: 500,
         },
 
+        // Profile
+
+        avatar: {
+          height: 200,
+          width: 200,
+          resizeMode: 'contain',
+          marginBottom: 10,
+        },
+
         //// Calender
+
+        calender: {
+          width: '78%',
+        },
+        calenderCell: {
+          width: '20%',
+          height: 55,
+        },
 
         // Dictionary 4.7 inch
         dictItemImage: {
@@ -653,8 +673,8 @@ export const customStyles = StyleSheet.create(
 
         // Dictionary
         dictItemImage: {
-          width: 95,
-          height: 95,
+          width: 85,
+          height: 85,
           left: 10,
           resizeMode: 'contain',
         },
